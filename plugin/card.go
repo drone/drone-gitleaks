2dd8769f152eb4b1d05e508e0e713e9b5424da97
Eoin McAfee <eoin.mcafee@harness.io>
2022-01-24 15:57:42 +0000

    extend plugin to publish adaptive card

---
+++ plugin/card.go
@@ -0,0 +1,41 @@
+package plugin
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/drone/drone-go/drone"
+)
+
+func (args Args) writeCard(dat []match) error {
+	result, _ := json.Marshal(CardIssues{Issues: dat})
+	card := drone.CardInput{
+		Schema: "https://drone-plugins.github.io/drone-gitleaks/card.json",
+		Data:   result,
+	}
+	writeCard(args.CardFilePath, &card)
+	return nil
+}
+
+func writeCard(path string, card interface{}) {
+	data, _ := json.Marshal(card)
+	switch {
+	case path == "/dev/stdout":
+		writeCardTo(os.Stdout, data)
+	case path == "/dev/stderr":
+		writeCardTo(os.Stderr, data)
+	case path != "":
+		ioutil.WriteFile(path, data, 0644)
+	}
+}
+
+func writeCardTo(out io.Writer, data []byte) {
+	encoded := base64.StdEncoding.EncodeToString(data)
+	io.WriteString(out, "\u001B]1338;")
+	io.WriteString(out, encoded)
+	io.WriteString(out, "\u001B]0m")
+	io.WriteString(out, "\n")
+}
